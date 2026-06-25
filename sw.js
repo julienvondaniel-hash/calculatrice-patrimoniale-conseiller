6e0471ca9dcc514239c08bc63f6a18c4d9f410b1
julienvondaniel-hash <julienvondaniel@gmail.com>
2026-06-25 11:58:49 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'monkappro-v11';
+const CACHE = 'monkappro-v13';
 const ASSETS = ['./', './index.html', './app.js', './simulateurs.js', './config.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 // Recrée une réponse "propre" (sans redirection) — corrige l'erreur Safari
